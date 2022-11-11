2488a671a6ada39210bc5a0760435484c3c53b91
Alexander Ng <alexng353@gmail.com>
2022-11-11 13:42:10 -0800

    auth in a seperate file, prisma updates

--- server/src/app.ts
+++ server/src/app.ts
@@ -16,6 +16,8 @@ dotenv.config();
 
 const app = express();
 
+app.use("/", require("./auth"));
+
 app.use(
   cors({
     origin: "*",
@@ -26,152 +28,6 @@ app.use(bodyParser.json());
 
 app.use("/locales", express.static("locales"));
 app.use("/static", express.static("static"));
-
-app.post("/api/signup", async (req, res) => {
-  const inviteKey = req.body.inviteKey;
-  const basic = req.headers.authorization;
-
-  if (!basic) {
-    res.status(401).send("No basic auth");
-    return;
-  }
-
-  if (!inviteKey) {
-    res.status(401).send("No invite key");
-    return;
-  }
-
-  const stringified = Buffer.from(basic.split(" ")[1], "base64").toString(
-    "utf-8"
-  );
-
-  const [password, email] = stringified.split(":");
-  // check if invite key is valid
-
-  const invite = await prisma.invite.findUnique({
-    where: {
-      invite: inviteKey,
-    },
-  });
-
-  if (!invite) {
-    res.status(401).send("Invalid invite key");
-    return;
-  }
-
-  if (invite.Used) {
-    res.status(401).send("Invite key already used");
-    return;
-  }
-
-  // create user
-  const user = await prisma.user.create({
-    data: {
-      email: email,
-      password: crypto.createHash("sha512").update(password).digest("hex"),
-      invitedBy: {
-        connect: {
-          id: invite.id,
-        },
-      },
-    },
-  });
-  await prisma.invite.update({
-    where: {
-      id: invite.id,
-    },
-    data: {
-      Used: true,
-    },
-  });
-
-  return res.status(200).send("User created");
-});
-
-app.post("/api/login", async (req, res) => {
-  // get Basic Auth credentials
-  const basic = req.headers.authorization;
-
-  if (!basic) {
-    res.status(401).send("Unauthorized");
-
-    return;
-  }
-
-  const [password, email] = Buffer.from(basic.split(" ")[1], "base64")
-    .toString("utf-8")
-    .split(":");
-
-  const user = await prisma.user.findUnique({
-    where: {
-      email: email,
-    },
-  });
-
-  if (!user) {
-    res.status(401).send("Unauthorized");
-    return;
-  }
-
-  const hashedPassword = crypto
-    .createHash("sha512")
-    .update(password)
-    .digest("hex");
-
-  if (user.password !== hashedPassword) {
-    res.status(401).send("Unauthorized");
-    return;
-  }
-
-  return res.status(200).json({ message: "success" });
-});
-
-app.post("/api/newinvite", async (req, res) => {
-  const basic = req.headers.authorization;
-
-  if (!basic) {
-    res.status(401).send("Unauthorized");
-    return;
-  }
-
-  const [password, email] = Buffer.from(basic.split(" ")[1], "base64")
-    .toString("utf-8")
-    .split(":");
-
-  const user = await prisma.user.findUnique({
-    where: {
-      email: email,
-    },
-  });
-
-  if (!user) {
-    res.status(401).send("Unauthorized");
-    return;
-  }
-
-  const hashedPassword = crypto
-    .createHash("sha512")
-    .update(password)
-    .digest("hex");
-
-  if (user.password !== hashedPassword) {
-    res.status(401).send("Unauthorized");
-    return;
-  }
-
-  const invite = await prisma.invite.create({
-    data: {
-      invite: crypto.randomBytes(16).toString("hex"),
-      CreatedBy: {
-        connect: {
-          id: user.id,
-        },
-      },
-    },
-  });
-
-  return res.status(200).json({ invite: invite.invite });
-});
 
 app.post("/api/post", async (req, res) => {
   const basic = req.headers.authorization;
@@ -206,6 +62,55 @@ app.post("/api/post", async (req, res) => {
     res.status(401).send("Unauthorized");
     return;
   }
+
+  const { namespace, language, content } = req.body;
+
+  // write content
+
+  const path = `./locales/${language}/${namespace}.json`;
+
+  fs.writeFile(path, content, (err) => {
+    if (err) {
+      logger.error(err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+  });
+
+  let db_namespace = await prisma.namespace.findUnique({
+    where: {
+      name: namespace,
+    },
+  });
+
+  if (!db_namespace) {
+    db_namespace = await prisma.namespace.create({
+      data: {
+        name: namespace,
+        content: content,
+        language: language,
+        path: path,
+      },
+    });
+  }
+
+  const post = await prisma.edit.create({
+    data: {
+      language: language,
+      namespace: namespace,
+      user: {
+        connect: {
+          id: user.id,
+        },
+      },
+      Namespace: {
+        connect: {
+          id: db_namespace.id,
+        },
+      },
+    },
+  });
+  return res.status(200).json(post);
 });
 
 app.get("*", async (req, res) => {
---
+++ server/src/auth.ts
@@ -0,0 +1,160 @@
+import express from "express";
+
+const router = express.Router();
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+import crypto from "crypto";
+import bodyParser from "body-parser";
+
+router.use(bodyParser.json());
+
+router.post("/api/signup", async (req, res) => {
+  const inviteKey = req.body.inviteKey;
+  const basic = req.headers.authorization;
+
+  if (!basic) {
+    res.status(401).send("No basic auth");
+    return;
+  }
+
+  if (!inviteKey) {
+    res.status(401).send("No invite key");
+    return;
+  }
+
+  const stringified = Buffer.from(basic.split(" ")[1], "base64").toString(
+    "utf-8"
+  );
+
+  const [password, email] = stringified.split(":");
+  // check if invite key is valid
+
+  const invite = await prisma.invite.findUnique({
+    where: {
+      invite: inviteKey,
+    },
+  });
+
+  if (!invite) {
+    res.status(401).send("Invalid invite key");
+    return;
+  }
+
+  if (invite.Used) {
+    res.status(401).send("Invite key already used");
+    return;
+  }
+
+  // create user
+  const user = await prisma.user.create({
+    data: {
+      email: email,
+      password: crypto.createHash("sha512").update(password).digest("hex"),
+      invitedBy: {
+        connect: {
+          id: invite.id,
+        },
+      },
+    },
+  });
+  await prisma.invite.update({
+    where: {
+      id: invite.id,
+    },
+    data: {
+      Used: true,
+    },
+  });
+
+  return res.status(200).send("User created");
+});
+
+router.post("/api/login", async (req, res) => {
+  // get Basic Auth credentials
+  const basic = req.headers.authorization;
+
+  if (!basic) {
+    res.status(401).send("Unauthorized");
+
+    return;
+  }
+
+  const [password, email] = Buffer.from(basic.split(" ")[1], "base64")
+    .toString("utf-8")
+    .split(":");
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  if (!user) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+
+  const hashedPassword = crypto
+    .createHash("sha512")
+    .update(password)
+    .digest("hex");
+
+  if (user.password !== hashedPassword) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+
+  return res.status(200).json({ message: "success" });
+});
+
+router.post("/api/newinvite", async (req, res) => {
+  const basic = req.headers.authorization;
+
+  if (!basic) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+
+  const [password, email] = Buffer.from(basic.split(" ")[1], "base64")
+    .toString("utf-8")
+    .split(":");
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  if (!user) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+
+  const hashedPassword = crypto
+    .createHash("sha512")
+    .update(password)
+    .digest("hex");
+
+  if (user.password !== hashedPassword) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+
+  const invite = await prisma.invite.create({
+    data: {
+      invite: crypto.randomBytes(16).toString("hex"),
+      CreatedBy: {
+        connect: {
+          id: user.id,
+        },
+      },
+    },
+  });
+
+  return res.status(200).json({ invite: invite.invite });
+});
+
+module.exports = router;
